ed6cc6dc40280bf8613a8a0ac9052af291444f60
Vincent Santos <vinnysantos559@gmail.com>
2026-03-13 22:26:03 -0700

    Add critic folder to clear history script

--- scripts/clear-history.sh
+++ scripts/clear-history.sh
@@ -10,6 +10,8 @@ rm -rf $BASE/data/ipc/**
 rm -rf $BASE/groups/shared/mission-control/**
 rm -rf "$BASE/groups/homie/briefings/"
 rm -rf "$BASE/groups/homie/logs/"
+rm -rf "$BASE/groups/critic/logs/"
+rm -rf $BASE/groups/critic/critiques/**
 rm -rf "$BASE/groups/worker/logs/"
 rm -rf "$BASE/groups/verifier/logs/"
 rm -rf "$BASE/groups/shared/logs/"
